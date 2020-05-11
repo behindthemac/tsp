fbd871ea599172edcbdde48e5163bb38092c18fb
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2020-05-11 10:01:41 +0900

    find the shortest route

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ def main():
     d = [[distance.euclidean(c[i], c[j]) for j in range(n)] for i in range(n)]
 
     routes = circular_permutations(list(range(n)))
+    shortest_route = next(routes)
+    shortest_length = get_length(d, shortest_route)
+    for route in routes:
+        length = get_length(d, route)
+        if length < shortest_length:
+            shortest_route = route
+            shortest_length = length
+
+
+def get_length(d, route):
+    length = 0
+    for i, j in zip(route, np.roll(route, -1)):
+        length += d[i][j]
+    return length
 
 
 if __name__ == '__main__':
