2523ef0a2d4659fb3c3b3ebbe16fc78984b4e391
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2020-05-12 20:24:22 +0900

    show the shortest route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial import distance
 from permutations import circular_permutations
 
@@ -17,12 +18,27 @@ def main():
             shortest_route = route
             shortest_length = length
 
+    show_route(c, shortest_route)
+
 
 def get_length(d, route):
     length = 0
     for i, j in zip(route, np.roll(route, -1)):
         length += d[i][j]
     return length
+
+
+def show_route(c, route):
+    plt.scatter(c[:, 0], c[:, 1])
+    r = route + [route[0]]
+    x = rearrange(c[:, 0], r)
+    y = rearrange(c[:, 1], r)
+    plt.plot(x, y)
+    plt.show()
+
+
+def rearrange(array, order):
+    return [array[i] for i in order]
 
 
 if __name__ == '__main__':
